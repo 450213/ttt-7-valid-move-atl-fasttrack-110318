b04eec177b7d49c3fd0700ef24fea71bba36ea04
Ila Leigh Baugham <leigh.baugham@gmail.com>
2018-11-04 19:57:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move? (board, index)
   if board[index] == " "|| board[index]==""|| board[index]==nil
-    return truth
+    return true
   else if board[index]== "X"|| board[index]== "O"
     return true
   end
